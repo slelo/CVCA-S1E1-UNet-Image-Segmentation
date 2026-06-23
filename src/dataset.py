61d1ca183b8a0f4ab83796ff2680c345b6a925ef
sla-lev <e1634250@student.tuwien.ac.at>
2026-06-23 15:23:45 +0200

    Initial commit with model structure

---
+++ src/dataset.py
@@ -0,0 +1,63 @@
+import os
+import json
+import numpy as np
+import pandas as pd
+import cv2
+import torch
+from torch.utils.data import Dataset
+
+# Dataset class for Coco style segmentation
+class CocoSegDataset(Dataset):
+    # Initialize the dataset
+    def __init__(self, image_dir, json_path):
+        self.image_dir = image_dir
+
+        # Load annotations
+        with open(json_path, 'r') as f:
+            data = json.load(f)
+            self.images = data['images']
+            self.annotations = data['annotations']
+
+            # build index: image_id to annotations
+            self.ann_map = {}
+            for ann in self.annotations:
+                img_id = ann['image_id']
+                self.ann_map.setdefault(img_id, []).append(ann)
+
+            self.id_to_image = {img['id']: img for img in self.images}
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        img_info = self.images[idx]
+        img_id = img_info['id']
+
+        file_name = img_info['file_name']
+        img_path = os.path.join(self.image_dir, file_name)
+
+        # Load image
+        image = cv2.imread(img_path)
+        print(image.shape)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = image.astype(np.float32)
+
+        # Get image size
+        H, W = img_info['height'], img_info['width']
+
+        # create empty mask. We will use this later for image segmentation
+        mask = np.zeros((H,W), dtype=np.uint8)
+
+        # draw polygons
+        anns = self.ann_map.get(img_id, [])
+
+        for ann in anns:
+            for seg in ann["segmentation"]:
+                pts = np.array(seg).reshape(-1, 2).astype(np.int32)
+                cv2.fillPoly(mask, [pts], 1)
+
+        # turn them into tensors using PyTorch
+        image = torch.from_numpy(image).permute(2, 0, 1).float()
+        mask = torch.from_numpy(mask).float().unsqueeze(0).float()
+
+        return image, mask
---
+++ src/model.py
@@ -0,0 +1,84 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+
+        # Convolutional layers where self.net is a sequential container of layers
+        self.net = nn.Sequential(
+            nn.Conv2D(in_channels, out_channels, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels, out_channels, 3, padding=1),
+            nn.ReLU(inplace=True),
+        )
+
+    # Forward pass
+    def forward(self, x):
+        return self.net(x)
+
+class EncoderBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        # Convolutional layers
+        self.conv = DoubleConv(in_channels, out_channels)
+        # Max Pooling
+        self.pool = nn.MaxPool2d(2)
+
+    def forward(self, x):
+        # x is the input feature map
+        x = self.conv(x)
+        # p is the downsampled feature map
+        p = self.pool(x)
+        return x, p
+
+class DecoderBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        # Upsample by transposed convolution
+        self.up = nn.ConvTranspose2d(in_channels, out_channels, 2, stride=2)
+        # Convolutional layers
+        self.conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, x, skip):
+        x = self.up(x)
+        x = torch.cat([x, skip], dim=1)
+        x = self.conv(x)
+        return x
+
+
+class UNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.enc1 = EncoderBlock(3, 64)
+        self.enc2 = EncoderBlock(64, 128)
+        self.enc3 = EncoderBlock(128, 256)
+
+        self.bottleneck = DoubleConv(256, 512)
+
+        self.dec1 = DecoderBlock(512, 256)
+        self.dec2 = DecoderBlock(256, 128)
+        self.dec3 = DecoderBlock(128, 64)
+
+        self.out = nn.Conv2d(64, 1, 1)
+
+    # Forward pass. This method does the following:
+    # 1. Pass the input through the encoder blocks
+    # 2. Pass the output of the last encoder block through the bottleneck
+    # 3. Pass the output of the bottleneck through the decoder blocks
+    # 4. Pass the output of the last decoder block through the output layer
+    def forward(self, x):
+        s1, p1 = self.enc1(x)
+        s2, p2 = self.enc2(p1)
+        s3, p3 = self.enc3(p2)
+
+        b = self.bottleneck(p3)
+
+        # d2 and d3 take x as parameter, because:
+        d1 = self.dec1(b, s3)
+        d2 = self.dec2(x, s2)
+        d3 = self.dec3(x, s1)
+
+        return self.out()
