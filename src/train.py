ef807efcfc2d863c44af5d20051202f376e2c08c
sla-lev <e1634250@student.tuwien.ac.at>
2026-06-30 16:10:06 +0200

    Added IoU output and documentation

--- src/train.py
+++ src/train.py
@@ -12,14 +12,32 @@ import argparse
 
 path = "../checkpoints"
 
-#CONTROLS
-#set the range of the images here (0 means all image should be used for training)
+# CONTROLS
+# DO NOT change them here, pass them instead as parameters in the Jupyter Notebooks
+# train_range sets the range of the images to
+# train the model (0 means all image should be used for training)
 train_range = 0
+# batch_size hyperparameter controls the number of training examples
+# processed by a model before the model's weights are updates
 batch_size = 8
+# num_epochs hyperparameter controls how many training cycles we
+# want to have over the range of train_range
 num_epochs = 200
+# CUDA should be used if possible to accelerate training. Change it to False in case
+# of unexpected errors or for debugging
 use_cuda =True
 
+
 def compute_iou(logits, masks, threshold=0.5, eps=1e-6):
+    """
+    Intersection over Union metric. Very useful for labeling tasks
+    Learn more: https://pyimagesearch.com/2016/11/07/intersection-over-union-iou-for-object-detection/
+    :param logits: raw logits of the model from the output layer
+    :param masks: array of ground truth masks
+    :param threshold: the lower limit for a logit value to consider the pixel as a vehicle
+    :param eps: small constant value to avoid zero-division
+    :return:
+    """
     probs = torch.sigmoid(logits)
     preds = (probs > threshold).float()
 
@@ -29,34 +47,64 @@ def compute_iou(logits, masks, threshold=0.5, eps=1e-6):
     iou = (intersection + eps) / (union + eps)
     return iou.item()
 
+
 def train_one_epoch(model, loader, optimizer, criterion, device):
+    """
+    # Training one epoch based on the defined parameters.
+    :param model: The model we built
+    :param loader: Dataloader of split dataset for training (see main())
+    :param optimizer: The optimzer we use to adjust the weights and learning rate of neural network.
+    In this project we use Adam.
+    :param criterion: Our loss function (Criterion is a PyTorch abstraction to standardize loss functions,
+    an instance of torch.nn. See more: https://docs.pytorch.org/docs/2.12/nn.html#loss-functions)
+    :param device: The device we use to run the training. In our case CUDA or CPU
+    :return: epoch_loss and epoch_iou
+    """
     model.train()
     running_loss = 0.0
+    running_iou = 0.0
 
     for images, masks in loader:
 
         images = images.to(device)
         masks = masks.to(device)
 
-        #forward
+        #forward propagation
         logits = model(images)
-
         loss = criterion(logits, masks)
 
-        #backward
+        iou = compute_iou(logits, masks)
+
+        #backward propagation
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-        running_loss += loss.item() * images.size(0)
+        #calculating the loss
+        running_loss += loss.item() * batch_size
+        running_iou += iou * batch_size
+
 
     epoch_loss = running_loss / len(loader.dataset)
-    return epoch_loss
+    epoch_iou = running_iou / len(loader.dataset)
 
-@torch.no_grad()
+    return epoch_loss, epoch_iou
+
+@torch.no_grad() # this decorator tells PyTorch not to keep track of the gradients. We only need that for training.
 def validate_one_epoch(model, loader, criterion, device):
+    """
+    Validating model training.
+    :param model: The model we built
+    :param loader: Dataloader of split dataset for training (see main())
+    :param criterion: Our loss function (Criterion is a PyTorch abstraction to standardize loss functions,
+    an instance of torch.nn. See more: https://docs.pytorch.org/docs/2.12/nn.html#loss-functions)
+    :param device: The device we use to run the training. In our case CUDA or CPU
+    :return: epoch_loss and epoch_iou
+    :return:
+    """
     model.eval()
     running_loss = 0.0
+    running_iou = 0.0
 
     for images, masks in loader:
         images = images.to(device)
@@ -67,18 +115,21 @@ def validate_one_epoch(model, loader, criterion, device):
 
         iou = compute_iou(logits, masks)
 
-        running_loss += loss.item() * images.size(0)
+        # calculating the loss
+        running_loss += loss.item() * batch_size
+        running_iou += iou * batch_size
 
     epoch_loss = running_loss / len(loader.dataset)
+    epoch_iou = running_iou / len(loader.dataset)
 
-    return epoch_loss
+    return epoch_loss, epoch_iou
 
 def main(args):
     train_range = args.train_range
     batch_size = args.batch_size
     num_epochs = args.num_epochs
     use_cuda = args.use_cuda
-    print("use_cuda is now ", use_cuda)
+    print("Value of use_cuda is now ", use_cuda)
 
     print("Running training with "
           "train_range=", train_range,
@@ -112,6 +163,7 @@ def main(args):
     val_size = int(len(full_dataset)*val_ratio)
     train_size = len(full_dataset) - val_size
 
+    #generates random numbers
     generator = torch.Generator().manual_seed(random_seed)
     train_dataset, val_dataset = random_split(
         full_dataset,
@@ -143,23 +195,34 @@ def main(args):
     # training loop
     # -------------
     best_val_loss = float("inf")
+    best_val_iou = float("inf")
 
     for epoch in range(num_epochs):
-        train_loss = train_one_epoch(model, train_loader, optimizer, criterion, device)
-        val_loss = validate_one_epoch(model, val_loader, criterion, device)
+        train_loss, train_iou = train_one_epoch(model, train_loader, optimizer, criterion, device)
+        val_loss, val_iou = validate_one_epoch(model, val_loader, criterion, device)
 
         print(
             f"Epoch [{epoch+1}/{num_epochs}] | "
             f"train_loss: {train_loss:.4f} | "
-            f"val_loss: {val_loss:.4f}"
+            f"val_loss: {val_loss:.4f} | "
+            f"train_iou: {train_iou:.4f} | "
+            f"val_iou: {val_iou:.4f}"
         )
 
-        # save the best model
+        # save the model with best loss
         if val_loss < best_val_loss:
             best_val_loss = val_loss
             Path(path).mkdir(parents=True, exist_ok=True)
-            torch.save(model.state_dict(), path+"/best_model.pth")
-            print("Saved best model")
+            torch.save(model.state_dict(), path+"/best_loss_model.pth")
+            print("Saved best LOSS model")
+
+        # save the model with best iou
+        if val_iou < best_val_iou:
+            best_val_iou = val_iou
+            Path(path).mkdir(parents=True, exist_ok=True)
+            torch.save(model.state_dict(), path + "/best_iou_model.pth")
+            print("Saved best IoU model")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
